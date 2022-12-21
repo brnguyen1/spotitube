b51c08621fbaa9762892fb841fd96b947163b29e
bnguyen <bnguyen@tamu.edu>
2022-12-20 19:45:41 -0600

    refactored spotify part and stored access token in session

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
-var sessions = require('express-session')
-
-var indexRouter = require('./routes/index');
-var spotifyRouter = require('./routes/spotify');
-var youtubeRouter = require('./routes/youtube');
+var sessions = require('cookie-session')
 
 var app = express();
 const dotenv = require('dotenv');
@@ -17,23 +13,21 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
-
-// creating 24 hours from milliseconds
-const oneDay = 1000 * 60 * 60 * 24;
 
 //session middleware
 app.use(sessions({
-    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
-    cookie: {
-        maxAge: oneDay,
-        httpOnly: false
-    },
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
 }));
 
-app.use('/', indexRouter);
-app.use('/callback', spotifyRouter);
+
+// Router
+var spotifyRouter = require('./routes/spotify');
+var youtubeRouter = require('./routes/youtube');
+
+app.use('/spotify', spotifyRouter);
 app.use('/youtube', youtubeRouter);
 
 module.exports = app;
--- backend/routes/youtube.js
+++ backend/routes/youtube.js
@@ -1,15 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var api = require('../services/spotify-api')
+var api = require('../services/youtube-api')
 
-ytToken = '';
-tracksSearch = [];
-playlistName = 'lifted'
-
-router.get('/', function(req, res) {
-    console.log(api.ytAuthUrl);
-    res.redirect(api.ytAuthUrl);
+router.get('/', function (req, res) {
+    var a_link = api.youtube_link();
+    res.send(a_link)
 });
+
 
 router.get('/auth-code', function(req, res) {
     ssn = req.session
